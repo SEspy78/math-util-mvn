689e824468a804cde9f448273641004c327004ed
SEspy78 <baokhang212121@gmail.com>
2023-10-16 11:26:42 +0700

    update getf() to use recursion and to see green4ever

--- src/main/java/com/baokhang/mathutil/core/MathUtility.java
+++ src/main/java/com/baokhang/mathutil/core/MathUtility.java
@@ -12,17 +12,37 @@ package com.baokhang.mathutil.core;
 public class MathUtility {
     public static final double PI =3.14;
     //public const double PI=3.14; c#
-    public static long getFactorial(int n){
+//    public static long getFactorial(int n){
+//        if(n<0 || n>20)
+//            throw  new IllegalArgumentException("Invalid n. n must be 0..20");
+//        if(n==0 || n==1)
+//            return 1;
+//        long result =1;
+//        for (int i = 2; i <= n; i++) {
+//            result *=i;
+//        }
+//    
+//        return result;
+//    }
+     public static long getFactorial(int n){
         if(n<0 || n>20)
-            throw  new IllegalArgumentException("Invalid n. n must be 0..20");
-        if(n==0 || n==1)
+           throw  new IllegalArgumentException("Invalid n. n must be 0..20");
+      if(n==0 || n==1)
             return 1;
-        long result =10;
-        for (int i = 2; i <= n; i++) {
-            result *=i;
-        }
-    
-        return result;
-    }
-    
+      return n* getFactorial(n-1);
+     }
 }
+ //Dùng đệ quy 
+ // KĨ THUẬT KIỂM THỬ GỌI LÀ : REGRESSION TESTING     
+//                             KIỂM THỬ HỒI QUY -TEST LẠI NHỮNG THỨ ĐÃ TỪNG TEST
+// TRONG THỰC TẾ LÀM DỰ ÁN       
+// SỬA VÌ 
+// FIX BUG THÌ PHẢI SỬA CODE 
+// THÊM HÀM, THÊM TÍNH NĂNG 
+// TỐI ƯU HÀM CŨ, LÀM HÀM CŨ GỌN HƠN, DỄ ĐỌC HƠN, CHẠY NHANH HƠN 
+// HÀM ĐANG NGON, MÀU XANH 
+
+
+
+
+     
